12f21aa6871bf37983923b41006b041f5653bc91
andreimacavei <andrei_gabriel.macavei@cti.pub.ro>
2013-12-15 11:43:00 +0200

    Added .gitignore and another app

--- reader/models.py
+++ reader/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class FeedUrl(models.Model):
+    name = models.CharField(max_length = 100, blank = True)
+    url = models.URLField(max_length = 200, unique = True);
+    lastVisited = models.DateTimeField(auto_now = True)
+
+    def __unicode__(self):
+        if not self.name:
+            return self.url
+        else:
+            return self.name
