7c65981a72ebd5d43c84f3654e1a378d87474bed
andreimacavei <andrei_gabriel.macavei@cti.pub.ro>
2013-12-15 12:55:57 +0200

    Added model for user

--- reader/import_rss.py
+++ reader/import_rss.py
@@ -7,3 +7,39 @@ import json
 from bs4 import BeautifulSoup, UnicodeDammit
 from bs4.element import Tag
 
+def download(url):
+    html = urlopen(url).read()
+    soup = BeautifulSoup(html, "html.parser")
+    channel = soup.find("channel")
+
+    items = [item for item in channel.find_all("item")]
+
+    results = []
+
+    for item in items:
+        # Check if we get empty items
+        if item.find('\n') == 0:
+            import pdb; pdb.set_trace()
+
+        entry = {}
+        entry['title'] = item.title.get_text()
+        entry['description'] = item.description.get_text()
+        entry['link'] = item.link.get_text()
+        entry['pubdate'] = item.pubdate.get_text()
+
+        results.append(entry)
+
+    return results
+
+def write_json(filename, results):
+    f = open(filename, "w")
+    # f.write(codecs.BOM_UTF8)
+    json.dump(results, f, indent=4, encoding="utf8")
+    f.close()
+
+if __name__ == '__main__':
+
+    url = "http://feeds.bbci.co.uk/news/rss.xml"
+    records = download(url)
+    filename = "file.json"
+    write_json(filename, records)
