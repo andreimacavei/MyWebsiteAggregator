b195975969c12c3c082e2b622e10951c98338ecc
andreimacavei <andrei_gabriel.macavei@cti.pub.ro>
2013-12-15 11:44:49 +0200

    removed .pyc files

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -125,6 +125,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'books',
     'polls',
+    'reader',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
