ff7063eb4220f365a6c8bc6c8e09a2af420a9b9c
zaboco <zaboco@gmail.com>
2013-12-15 12:56:23 +0200

    add reader viewer html

--- reader/views.py
+++ reader/views.py
@@ -1,6 +1,21 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render
 
+dummyFeeds = [
+    {
+        'title': 'First Article',
+        'description': 'Some desc',
+        'link': '#',
+        'pubdate': '15/12/2013'
+    },
+    {
+        'title': 'Second Article',
+        'description': 'Other desc',
+        'link': '#',
+        'pubdate': '14/12/2013'
+    }
+]
 
 def homepage(request):
-    return HttpResponse("Webdev RSS Reader Wannabe")
+    return render(request, 'index.html', {'feeds': dummyFeeds})
